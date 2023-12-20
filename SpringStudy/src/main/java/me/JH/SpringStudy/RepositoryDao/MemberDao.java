6ad114be5c1e25fa8d811d55f8974b1cc06bd8d1
Nanoruri <kaby1217@gmail.com>
2023-12-20 16:10:05 +0900

    Signup page properties JPA, MariaDB/ JPA에 맞게 Dao클래스 일부 조정과 그에 따른 서비스 로직 수정

--- SpringStudy/src/main/java/me/JH/SpringStudy/RepositoryDao/MemberDao.java
+++ SpringStudy/src/main/java/me/JH/SpringStudy/RepositoryDao/MemberDao.java
@@ -6,17 +6,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberDao extends JpaRepository<User,String> {
+//이러면 JPA 쓰는 의미가 있는가..
 
-
-    // 특정 조건에 따른 회원 검색 메서드
-    User findByUserId(String userId);
-
-    //중복확인 검사 메서드/JPA는 메서드 이름을 기준으로  분석하여 쿼리를 자동으로 생성함. 그래서 엔티티 테이블과 매핑할 수 있는
+    //JPA는 메서드 이름을 기준으로  분석하여 쿼리를 자동으로 생성함. 그래서 엔티티 테이블과 매핑할 수 있는
     //엔티티 클래스가 필요한거라고 추즉 됨
-    boolean existsByUserId(String userId);
 
+    //
     User findByNameAndEmail(String name, String email);
 
-    User findByNameAndEmailAndUserId(String name, String email, String userId);
+    //찾기 서비스
+    User findByUserIdAndNameAndEmail(String userId, String name, String email);
 
 }
